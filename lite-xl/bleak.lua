71bfaf930ed6b2a996bac5a84944c7470d92fb9e
vqnxiv <shiravqn@gmail.com>
2022-02-14 23:37:34 +0100

    lite-xl

---
+++ lite-xl/bleak.lua
@@ -0,0 +1,29 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#1F1F1F" }
+style.background2 = { common.color "#272727" }
+style.background3 = { common.color "#272727" }
+style.text = { common.color "#666666" }
+style.caret = { common.color "#c07020" }
+style.accent = { common.color "#d0d0d0" }
+style.dim = { common.color "#404040" }
+style.divider = { common.color "#272727" }
+style.selection = { common.color "#181818" }
+style.line_number = { common.color "#3D3D3D" }
+style.line_number2 = { common.color "#a3a3a3" }
+style.line_highlight = { common.color "#2e2e2e" }
+style.scrollbar = { common.color "#272727" }
+style.scrollbar2 = { common.color "#232323" }
+style.nagbar_text = { common.color "#000000" }
+
+style.syntax["normal"] = { common.color "#AFAFAF" }
+style.syntax["symbol"] = { common.color "#AFAFAF" }
+style.syntax["comment"] = { common.color "#606060" }
+style.syntax["keyword"] = { common.color "#91739B" }
+style.syntax["keyword2"] = { common.color "#91739B" }
+style.syntax["number"] = { common.color "#6A8CB5" }
+style.syntax["literal"] = { common.color "#667F58" }
+style.syntax["string"] = { common.color "#667F58" }
+style.syntax["operator"] = { common.color "#969696" }
+style.syntax["function"] = { common.color "#C49E66" }
